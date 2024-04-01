d60813785e5bd1b9c6fde085eb72fc49e8896531
emrah unal <actioninstem@gmail.com>
2024-04-01 11:47:46 -0500

    handle click option

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -2,20 +2,28 @@ import { useState } from "react";
 import data from "./data";
 
 export default function Accordian() {
-    const [selected, setSelected] = useState(null)
-    return <div className="Wrapper">
-        <div className="accordian">
-            {
-                data && data.length > 0 ? (data.map((dataItem) => (<div className="item">
-                    <div className="title">
-                        <h3>{dataItem.question}</h3>
-                        <span>+</span>
-                    </div>
-                </div>))
-                ) : (<div>No data found</div>)
-            }
+    const [selected, setSelected] = useState(null);
+    function handleSingleSelection(getCurrentId) {
+        console.log(getCurrentId);
+    }
+    return (
+        <div className="Wrapper">
+            <div className="accordian">
+                {data && data.length > 0 ? (
+                    data.map((dataItem) => (
+                        <div className="item">
+                            <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
+                                <h4>{dataItem.question}</h4>
+                                <span>+</span>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <div>No data found</div>
+                )}
+            </div>
         </div>
-    </div>
+    );
 }
 
 // https://www.youtube.com/watch?v=5ZdHfJVAY-s&ab_channel=freeCodeCamp.org   15 23 te kaldim
