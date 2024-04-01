7fb81c77881b8fa8a4c017929a980461ec4db1c6
emrah unal <actioninstem@gmail.com>
2024-04-01 00:28:15 -0500

    index.js updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Accordian from './components/accordian';
 
 function App() {
   return (
     <div className="App">
-
+      <Accordian />
     </div>
   );
 }
--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,5 +1,20 @@
-export default function () {
-    return <div>
-        Accordian
+import { useState } from "react";
+import data from "./data";
+
+export default function Accordian() {
+    const [selected, setSelected] = useState(null)
+    return <div className="Wrapper">
+        <div className="accordian">
+            {
+                data && data.length > 0 ?
+                    (data.map((dataItem) => (<div className="item">
+                        <div className="title">
+                            <h3>{dataItem.question}</h3>
+                            <span>+</span>
+                        </div>
+                    </div>))
+                    ) : (<div>No data found</div>)
+            }
+        </div>
     </div>
 }
