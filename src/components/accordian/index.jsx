8e685ca1f7c664b9359582494c702ba2d60912fe
emrah unal <actioninstem@gmail.com>
2024-04-06 23:46:17 -0500

    changed handle clicks

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -3,45 +3,48 @@ import data from "./data";
 import "./styles.css";
 
 export default function Accordian() {
-    const [selected, setSelected] = useState(null);
-    const [enableMultiSelection, setEnableMultiSelection] = useState(false);
-    const [multiple, setMultiple] = useState([]);
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
-    function handleSingleSelection(getCurrentId) {
-        setSelected(getCurrentId === selected ? null : getCurrentId);
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
 
-    function handleMultiSelection(getCurrentId)
+    function handleMultiSelection(getCurrentId) {
+      let copyMultiple = [...multiple];
+      const findIndexCurrentId = copyMultiple.indexOf(getCurrentId);
 
+      console.log(findIndexCurrentId);
     }
     return (
-        <div className="wrapper">
-            <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
-                Enable Multi Selection
-            </button>
-            <div className="accordian">
-                {data && data.length > 0 ? (
-                    data.map((dataItem) => (
-                        <div className="item">
-                            <div
-                                onClick={
-                                    enableMultiSelection
-                                    ? () => handleMultiSelection(dataItem.id) 
-                                    : () => handleSingleSelection(dataItem.id)}
-                                className="title">
-                                <h4>{dataItem.question}</h4>
-                                <span>+</span>
-                            </div>
-                            {selected === dataItem.id ? (
-                                <div className="content">{dataItem.answer}</div>
-                            ) : null}
-                        </div>
-                    ))
-                ) : (
-                    <div>No data found</div>
-                )}
-            </div>
+      <div className="wrapper">
+        <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+          Enable Multi Selection
+        </button>
+        <div className="accordian">
+          {data && data.length > 0 ? (
+            data.map((dataItem) => (
+              <div className="item">
+                <div
+                  onClick={
+                    enableMultiSelection
+                      ? () => handleMultiSelection(dataItem.id)
+                      : () => handleSingleSelection(dataItem.id)
+                  }
+                  className="title">
+                  <h4>{dataItem.question}</h4>
+                  <span>+</span>
+                </div>
+                {selected === dataItem.id ? (
+                  <div className="content">{dataItem.answer}</div>
+                ) : null}
+              </div>
+            ))
+          ) : (
+            <div>No data found</div>
+          )}
         </div>
+      </div>
     );
+  }
 }
-
-// https://www.youtube.com/watch?v=5ZdHfJVAY-s&ab_channel=freeCodeCamp.org   15 23 te kaldim
