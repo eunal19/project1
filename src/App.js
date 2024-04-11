6396946b710cd7dd8310d31a38fba0d87e9d4615
emrah unal <actioninstem@gmail.com>
2024-04-11 00:10:09 -0500

    test2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Accordion from './components/accordion';
 import RandomColor from './components/random-color';
 import Test1 from './components/test1/test1';
+import Test2 from './components/test2/test2';
+
 
 function App() {
   return (
@@ -9,7 +11,7 @@ function App() {
       <Accordion />
       <RandomColor />
       <Test1 />
-
+      <Test2 />
     </div>
   );
 }
---
+++ src/components/test2/test2.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Test2() {
+  return (
+    <div>
+      <h2>dsfasdfds</h2>
+    </div>
+  );
+}
