1180c610e11ed4ff193d04aeb9facfaf1c053396
emrah unal <actioninstem@gmail.com>
2024-04-11 00:07:07 -0500

    test1

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function RandomColor(params) {
+export default function RandomColor() {
   const [typeOfColor, setTypeOfColor] = useState("hex");
   const [color, setColor] = useState("#000000");
   return (
---
+++ src/components/test1/test1.jsx
@@ -0,0 +1,7 @@
+export default function Test1() {
+  return (
+    <div>
+      <h1>unal nasilsin</h1>
+    </div>
+  );
+}
