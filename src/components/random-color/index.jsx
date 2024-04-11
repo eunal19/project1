b7907b47f246d908c821fe24b71f1b89ac1824d3
emrah unal <actioninstem@gmail.com>
2024-04-10 23:29:00 -0500

    adding style

---
+++ src/components/random-color/index.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function RandomColor(params) {
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+  const [color, setColor] = useState("#000000");
+
+  function changeColor(params) {}
+  return (
+    <div className="container">
+      <button onClick={() => changeColor()}>Create HEX Color</button>
+      <button>Generate RGB Color</button>
+      <button>Generate Random Color</button>
+    </div>
+  );
+}
