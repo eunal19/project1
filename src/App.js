ff5ef2029f65de15b332d5d071cc4a947227bbe7
emrah unal <actioninstem@gmail.com>
2024-04-14 20:52:31 -0500

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
 import Accordion from './components/accordion';
 import RandomColor from './components/random-color';
-import Test1 from './components/test1/test1';
-import Test2 from './components/test2/test2';
-import Test3 from './components/test3/test3';
-import Test4 from './components/test4/test4';
+
 
 
 function App() {
@@ -12,10 +9,7 @@ function App() {
     <div className="App">
       {/* <Accordion /> */}
       <RandomColor />
-      <Test1 />
-      <Test2 />
-      <Test3 />
-      <Test4 />
+
     </div>
   );
 }
