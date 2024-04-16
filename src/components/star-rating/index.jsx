bf09a192c78283f4207313bcd41b6e43ad56a592
emrah unal <actioninstem@gmail.com>
2024-04-16 18:36:17 -0500

    changes

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 export default function StarRating({ noOfStars = 5 }) {
+
+  const [rating, setRating] = useState(0)
+  const [hover, setHover] = useState(0)
+
+  function handleClick(getCurrentIndex) {
+    console.log(getCurrentIndex);
+  }
+
+    function handleMouseEnter(getCurrentIndex) {
+        console.log(getCurrentIndex);
+
+  }
+
+    function handleMouseLeave(getCurrentIndex) {
+        console.log(getCurrentIndex);
+
+  }
+
   return (
     <div className="start-rating">
       {[...Array(noOfStars)].map((_, index) => {
+        index += 1
         return <FaStar key={index} />;
-        onclick = {};
-        onmouseenter = {};
-        onmouseleave = {};
+        onclick = {()=>handleClick(index)};
+        onmouseenter = {()=>handleMouseEnter(index)};
+        onmouseleave = {()=>handleMouseLeave(index)};
         size =  {40}
       })}
     </div>
