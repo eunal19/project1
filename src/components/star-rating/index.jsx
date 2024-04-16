39dc802f84d729384390c33abadf93f77f8fe049
emrah unal <actioninstem@gmail.com>
2024-04-16 18:18:57 -0500

    changes

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { FaStar } from "react-icons/fa";
 
-export default function StarRating() {
-  return <div></div>;
+export default function StarRating({ noOfStars = 5 }) {
+  return (
+    <div className="start-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        return <FaStar key={index} />;
+        onclick = {};
+        onmouseenter = {};
+        onmouseleave = {};
+        size =  {40}
+      })}
+    </div>
+  );
 }
