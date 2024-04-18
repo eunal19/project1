3ea1b690107327fdb59ec45c9e6e7d7749a31d38
emrah unal <actioninstem@gmail.com>
2024-04-17 23:32:09 -0500

    change some stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 // import Accordion from './components/accordion';
-import RandomColor from './components/random-color';
+// import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 
 
--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -2,33 +2,34 @@ import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 export default function StarRating({ noOfStars = 5 }) {
-
-  const [rating, setRating] = useState(0)
-  const [hover, setHover] = useState(0)
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
 
   function handleClick(getCurrentIndex) {
     console.log(getCurrentIndex);
   }
 
-    function handleMouseEnter(getCurrentIndex) {
-        console.log(getCurrentIndex);
-
+  function handleMouseEnter(getCurrentIndex) {
+    console.log(getCurrentIndex);
   }
 
-    function handleMouseLeave(getCurrentIndex) {
-        console.log(getCurrentIndex);
-
+  function handleMouseLeave(getCurrentIndex) {
+    console.log(getCurrentIndex);
   }
 
   return (
     <div className="start-rating">
       {[...Array(noOfStars)].map((_, index) => {
-        index += 1
-        return <FaStar key={index} />;
-        onlick = {()=>handleClick(index)};
-        onmouseenter = {()=>handleMouseEnter(index)};
-        onmouseleave = {()=>handleMouseLeave(index)};
-        size =  {40}
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            onClick={() => handleClick(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+            size={40}
+          />
+        );
       })}
     </div>
   );
