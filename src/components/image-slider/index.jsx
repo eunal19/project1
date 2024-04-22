a3c3fb69b5945decf368a92875edc397754d469d
emrah unal <actioninstem@gmail.com>
2024-04-21 23:35:49 -0500

    add functions

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -3,14 +3,37 @@ import React, { useEffect, useState } from "react";
 export default function ImageSlider({ url, limit }) {
   const [images, setImages] = useState([]);
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-async function fetchImages(getUrl) {
-  
-}
+  async function fetchImages(getUrl) {
+    try {
+      setLoading(true);
 
-  useEffect(() => 
-if(url != '') fetchImages(url)
+      const response = await fetch(getUrl);
+      const data = await response.json();
+
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (e) {
+      setErrorMsg(e.message);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
   }, [url]);
+
+  if (loading) {
+    return <div>Loading data! Please wait</div>;
+  }
+
+  if (errorMsg !== null) {
+    return <div>Error Occured! {errorMsg}</div>;
+  }
 
   return <div className="container"></div>;
 }
