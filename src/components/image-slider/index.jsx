729c71e5764c4d7f0c6bdb3211a9ec78ec7b220e
emrah unal <actioninstem@gmail.com>
2024-04-21 23:09:57 -0500

    add functions

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function ImageSlider() {
-  return <div></div>;
+export default function ImageSlider({ url, limit }) {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+async function fetchImages(getUrl) {
+  
+}
+
+  useEffect(() => 
+if(url != '') fetchImages(url)
+  }, [url]);
+
+  return <div className="container"></div>;
 }
