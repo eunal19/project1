88308d14044ecf624c86fe74e14a8e96fbe3e841
emrah unal <actioninstem@gmail.com>
2024-04-23 15:44:49 -0500

    update

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import ImageSlider from './components/image-slider';
 // import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 
-
-
-
 function App() {
   return (
     <div className="App">
