39fbe860fc9ef5d5f6a88f3380cb788feaefe204
emrah unal <actioninstem@gmail.com>
2024-04-26 00:02:38 -0500

    first

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import ImageSlider from './components/image-slider';
 // import Accordion from './components/accordion';
 // import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+// import StarRating from './components/star-rating';
 
 function App() {
   return (
