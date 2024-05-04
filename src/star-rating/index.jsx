1c283170003f4beeca0557136bc1677af4e7e853
emrah unal <actioninstem@gmail.com>
2024-05-03 23:17:05 -0500

    first

---
+++ src/star-rating/index.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import "./styles.css";
+
+export default function StarRating({ noOfStars = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(currentIndex) {
+    setRating(currentIndex);
+  }
+
+  function handleMouseEnter(currentIndex) {
+    setHover(currentIndex);
+  }
+
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
+  return (
+    <div className="start-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            size={45}
+          />
+        );
+      })}
+    </div>
+  );
+}
